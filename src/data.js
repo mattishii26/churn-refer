768fa6e6f76d4945826f84fa474c04b384daf41f
mattishii26 <chihiro2657@gmail.com>
2020-03-13 19:04:10 -0700

    More Data

--- src/data.js
+++ src/data.js
@@ -34,21 +34,80 @@ let data =
             ]
         },
         {
+            cardCompany: "Capital One",
+            creditCards: 
+            [
+                "Platinum",
+                "Venture Rewards",
+                "VentureOne Rewards",
+                "Quicksilver Rewards",
+                "Savor Rewards",
+                "SavorOne Rewards",
+                "QuicksilverOne Rewards",
+                "Journey Student Rewards",
+                "Secured Mastercard",
+                "Capital One Walmart Rewards Mastercard",
+                "Spark Cash",
+                "Spark Miles",
+                "Spark Cash Select",
+                "Spark Miles Select",
+                "Spark Classic",
+                "BuyPower Business Card",
+                "BuyPower Card",
+                "Cabela's CLUB Card",
+                "Bass Pro Shops CLUB Card"
+            ]
+        },
+        {
             cardCompany: "Chase",
             creditCards: 
             [
                 "Freedom",
                 "Freedom Unlimited",
-                "IHG Premier",
-                "Ink Preferred",
-                "Ink Unlimited",
+                "Slate",
+                "IHG Rewards Club Premier",
+                "IHG Rewards Club Traveler",
+                "Ink Business Preferred",
+                "Ink Business Unlimited",
+                "Ink Business Cash",
                 "Marriott Bonvoy Bold",
                 "Marriott Bonvoy Boundless",
                 "Sapphire Preferred",
-                "Sapphire Reserve"
+                "Sapphire Reserve",
+                "Southwest Rapid Rewards Priority",
+                "Southwest Rapid Rewards Plus",
+                "Southwest Rapid Rewards Premier",
+                "United Explorer",
+                "United TravelBank Card",
+                "United Club Card",
+                "British Airways Visa Signature Card",
+                "Aer Lingus Visa Signature Card",
+                "Iberia Visa Signature Card",
+                "World of Hyatt",
+                "Disney Premier Visa Card",
+                "Disney Visa Card",
+                "Starbucks Rewards Visa Card",
+                "Amazon Prime Rewards Visa Signature Card",
+                "AARP Credit Card",
+                "United Business Card",
+                "Southwest Rapid Rewards Performance Business",
+                "Southwest Rapid Rewards Premier Business"
             ]
         },
-
+        {
+            cardCompany: "Discover",
+            creditCards: 
+            [
+                "Discover it Cash Back",
+                "Discover it Travel",
+                "Discover it Gas & Restaurant",
+                "Discover it Secured",
+                "Discover it Student Cash Back",
+                "Discover it Student Chrome",
+                "NHL",
+                "Discover it Business"
+            ]
+        },
     ]
 }
 
